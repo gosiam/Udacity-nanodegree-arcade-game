25e971344bf6b9c82b85b22aafa7d696f203bf15
gosiam <gosiam@users.noreply.github.com>
2016-09-06 03:24:29 -0400

    Enemies now appear on the canvas

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    'use strict';
     this.x = x;
     this.y = y;
     this.speed = speed;
@@ -17,12 +18,20 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x += this.x + this.speed * dt;
+    this.x += this.speed * dt;
+    // when enemy bug reaches end of canvas call reset()
+  if (this.x > 505) {
+    this.reset();
+  }
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+// resets the enemy bug back to start of canvas
+Enemy.prototype.reset = function() {
+  this.x = -200;
 };
 
 // Now write your own player class
@@ -86,9 +95,24 @@ Player.prototype.reset = function() {
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+
 var allEnemies = [];
+
+// push enemies into allEnemies array, 4 enemies total
+// sets Y coordinate for each enemy to 0, 145, and 225 by using incrementer
+// sets X coordinate randomly
+// sets speed to a base of 50 and then randomizes each enemy
+for (var i = 0; i < 3; i++) {
+  var enemyY = 65 + 80 * i;
+  var enemyX = Math.floor(Math.random() * 30);
+  var enemySpeed = 50 + Math.floor(Math.random() * 150);
+  allEnemies.push(new Enemy(enemyX, enemyY, enemySpeed));
+}
+
+
 var player = new Player(200,400);
+
+
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
